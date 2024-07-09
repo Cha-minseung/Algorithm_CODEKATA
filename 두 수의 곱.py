7baf4fc05198b5b36162a413c3a586dcf23efbfd
Cha-minseung <getup0269@gmail.com>
2024-07-09 12:01:56 +0900

    두 수의 곱

---
+++ 두 수의 곱.py
@@ -0,0 +1,11 @@
+a = int(input("첫 번째 정수를 입력하세요: "))  # 정수 입력받기
+b = int(input("두 번째 정수를 입력하세요: "))
+
+
+def sum_multip(a, b):  # 함수 정의하기
+    return a*b
+
+
+result = (sum_multip(a, b))
+
+print("두 수의 곱은", result, "입니다.")
